cf9c49ab31004937e9efeff7b44fb3c506648588
Frans Peter Larsen <larsen.frans@gmail.com>
2018-11-12 12:01:05 +0100

    Added basic setup

---
+++ Assets/_Completed-Assets/Scripts/ML/TanksAgent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class TanksAgent : Agent {
+    public void Start()
+    {
+        
+    }
+
+    public override void AgentReset()
+    {
+
+    }
+}
+
